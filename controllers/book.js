d132c4ed84393ae26325eb358bbce7d421ca19cb
sarahameed <shirosara19@gmail.com>
2025-10-03 20:24:08 +0300

    feat: filter books by user and enhance book details display

--- controllers/book.js
+++ controllers/book.js
@@ -6,7 +6,8 @@ const User = require('../models/user.js');
 // INDEX - list all books
 router.get('/', async (req, res) => {
   try {
-    const books = await Book.find().populate('userId', 'username');
+    const books = await Book.find({ userId: req.session.user._id })
+      .populate('userId', 'username');
     res.render('books/index.ejs', { books });
   } catch (error) {
     console.log(error);
@@ -14,12 +15,21 @@ router.get('/', async (req, res) => {
   }
 });
 
-// NEW - form to add a new book
+// NEW - display form to create new book (Move this BEFORE :bookId routes)
 router.get('/new', (req, res) => {
+  res.render('books/new.ejs');
+});
+
+// DISCOVER - show all books
+router.get('/discover', async (req, res) => {
   try {
-    res.render('books/new.ejs');
+    const books = await Book.find()
+      .populate('userId', 'username')
+      .populate('reviews.userId', 'username');
+    res.render('books/discover.ejs', { books });
   } catch (error) {
     console.log(error);
+    res.redirect('/');
   }
 });
 
@@ -34,7 +44,7 @@ router.post('/', async (req, res) => {
 
     if (existingBook) {
       return res.render('books/new.ejs', {
-        error: 'This book already exists in our library!'
+        error: 'This book already exists in the library!'
       });
     }
 
@@ -50,43 +60,48 @@ router.post('/', async (req, res) => {
       { $push: { books: book._id } }
     );
 
-    res.redirect('/books');
+    res.redirect('/books?msg=Book added successfully!');
   } catch (error) {
     console.log(error);
-    res.redirect('/');
+    res.render('books/new.ejs', {
+      error: 'Error creating book. Please try again.'
+    });
   }
 });
 
-// SHOW - display a specific book
+// All routes with :bookId parameter come AFTER specific routes
 router.get('/:bookId', async (req, res) => {
   try {
-    const currentUser = await User.findById(req.session.user._id);
-    const book = currentUser.books.id(req.params.bookId);
-    res.render('books/show.ejs', { book });
-  } catch (error) {
-    console.log(error);
-    res.redirect('/books');
-  }
-});
+    const book = await Book.findById(req.params.bookId)
+      .populate('userId', 'username')
+      .populate('reviews.userId', 'username');
 
-// DELETE - remove a book
-router.delete('/:bookId', async (req, res) => {
-  try {
-    const currentUser = await User.findById(req.session.user._id);
-    currentUser.books.id(req.params.bookId).deleteOne();
-    await currentUser.save();
-    res.redirect('/books');
+    if (!book) {
+      return res.redirect('/books');
+    }
+
+    res.render('books/show.ejs', {
+      book,
+      isOwner: book.userId.toString() === req.session.user._id.toString()
+    });
   } catch (error) {
     console.log(error);
-    res.redirect('/');
+    res.redirect('/books');
   }
 });
 
 // EDIT - form to edit a book
 router.get('/:bookId/edit', async (req, res) => {
   try {
-    const currentUser = await User.findById(req.session.user._id);
-    const book = currentUser.books.id(req.params.bookId);
+    const book = await Book.findOne({
+      _id: req.params.bookId,
+      userId: req.session.user._id
+    });
+
+    if (!book) {
+      return res.redirect('/books');
+    }
+
     res.render('books/edit.ejs', { book });
   } catch (error) {
     console.log(error);
@@ -97,10 +112,46 @@ router.get('/:bookId/edit', async (req, res) => {
 // UPDATE - update a book
 router.put('/:bookId', async (req, res) => {
   try {
-    const currentUser = await User.findById(req.session.user._id);
-    const book = currentUser.books.id(req.params.bookId);
-    book.set(req.body);
-    await currentUser.save();
+    const book = await Book.findOneAndUpdate(
+      {
+        _id: req.params.bookId,
+        userId: req.session.user._id
+      },
+      req.body,
+      { new: true }
+    );
+
+    if (!book) {
+      return res.redirect('/books');
+    }
+
+    res.redirect('/books');
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+// DELETE - remove a book
+router.delete('/:bookId', async (req, res) => {
+  try {
+    const book = await Book.findOne({
+      _id: req.params.bookId,
+      userId: req.session.user._id
+    });
+
+    if (!book) {
+      return res.redirect('/books');
+    }
+
+    await Book.findByIdAndDelete(req.params.bookId);
+
+    // Remove book reference from user
+    await User.findByIdAndUpdate(
+      req.session.user._id,
+      { $pull: { books: req.params.bookId } }
+    );
+
     res.redirect('/books');
   } catch (error) {
     console.log(error);
@@ -113,10 +164,23 @@ router.post('/:bookId/reviews', async (req, res) => {
   try {
     const book = await Book.findById(req.params.bookId);
 
+    if (!book) {
+      return res.redirect('/books');
+    }
+
+    // Check if user already reviewed this book
+    const existingReview = book.reviews.find(
+      review => review.userId.toString() === req.session.user._id.toString()
+    );
+
+    if (existingReview) {
+      return res.redirect(`/books/${req.params.bookId}?error=You have already reviewed this book`);
+    }
+
     book.reviews.push({
       userId: req.session.user._id,
       username: req.session.user.username,
-      rating: req.body.rating,
+      rating: parseInt(req.body.rating),
       comment: req.body.comment
     });
 
